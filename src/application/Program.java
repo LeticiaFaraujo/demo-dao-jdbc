7ee93401fe3d613528ef09f03d79ef80c8c3826b
Leticia Ferreira <leticyafaraujo@gmail.com>
2019-03-15 23:07:29 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,7 +15,9 @@ public class Program {
 		
 		Seller seller = new Seller(21,"bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
-		System.out.println(seller);
+		SellerDao sellerdao = DaoFactory.createSellerDao(); 
+		
+		System.out.println(sellerdao);
 		
 	}
 
